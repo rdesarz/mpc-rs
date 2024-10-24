3009d2ea3a6b3c86460863e9fd3232b8a76dde68
Romain Desarzens <romain.desarzens@protonmail.com>
2024-10-24 21:44:41 +0200

    Add first lines of code

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+mod simulator
+{
+    #[derive(Debug)]
+    pub struct TwoWheeledSystem
+    {
+        pub x: f32,
+        pub y: f32,
+        pub yaw: f32,
+        pub v: f32
+    }
+
+    pub struct Command
+    {
+        pub u1: f32,
+        pub u2: f32
+    }
+
+    pub fn apply_differential_model(system: &mut TwoWheeledSystem, cmd: &Command, dt: f32)
+    {
+        let dx = system.yaw.cos() * system.v;
+        let dy = system.yaw.sin() * system.v;
+        let dv = cmd.u1;
+        let d_yaw = system.v / 0.25 * cmd.u2.sin();
+
+        system.x += dt * dx;
+        system.y += dt * dy;
+        system.yaw += dt * d_yaw;
+        system.v += dt * dv;
+    }
+}
+
+fn main() {
+    let mut system = simulator::TwoWheeledSystem{x: 0.0, y: 0.0, yaw: 0.0, v: 0.0};
+
+    println!("{:?}", system);
+    simulator::apply_differential_model(&mut system, &simulator::Command{u1: 1.0, u2: 0.0}, 0.5);
+    println!("{:?}", system);
+    simulator::apply_differential_model(&mut system, &simulator::Command{u1: 1.0, u2: 0.0}, 0.5);
+    println!("{:?}", system);
+}
+
