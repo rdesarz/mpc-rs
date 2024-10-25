655f1bf7024c9bba94a81088ec86dd18ac5e465b
Romain Desarzens <romain.desarzens@protonmail.com>
2024-10-25 18:35:10 +0200

    [WIP] Add plot

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use plotters::prelude::*;
+
 mod simulator
 {
     #[derive(Debug)]
@@ -29,7 +31,7 @@ mod simulator
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut system = simulator::TwoWheeledSystem{x: 0.0, y: 0.0, yaw: 0.0, v: 0.0};
 
     let command = simulator::Command{u1: 1.0, u2: 0.0};
@@ -42,5 +44,34 @@ fn main() {
         simulator::apply_differential_model(&mut system, &command, delta_t);
         println!("{:?}", system);
     }
+
+    let root = BitMapBackend::new("./test.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("y=x^2", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+            &RED,
+        ))?
+        .label("y = x^2")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
 }
 
