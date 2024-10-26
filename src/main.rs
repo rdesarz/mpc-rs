53ce884e3f0ba2d76b1bbbc15d601de8461b250c
Romain Desarzens <hib.pub@protonmail.com>
2024-10-26 15:29:35 +0200

    Improve drawing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use plotters::prelude::*;
 
 mod simulator
 {
-    #[derive(Debug)]
+    #[derive(Debug, Copy, Clone)]
     pub struct TwoWheeledSystem
     {
         pub x: f32,
@@ -31,6 +31,8 @@ mod simulator
     }
 }
 
+
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut system = simulator::TwoWheeledSystem{x: 0.0, y: 0.0, yaw: 0.0, v: 0.0};
 
@@ -39,9 +41,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Initial state : {:?}", system);
 
+    let mut trajectory = Vec::new();
     for _ in 1..100
     {
         simulator::apply_differential_model(&mut system, &command, delta_t);
+        trajectory.push(system.clone());
         println!("{:?}", system);
     }
 
@@ -52,23 +56,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .margin(5)
         .x_label_area_size(30)
         .y_label_area_size(30)
-        .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32)?;
+        .build_cartesian_2d(-50f32..50f32, -0.1f32..1f32)?;
 
     chart.configure_mesh().draw()?;
 
     chart
         .draw_series(LineSeries::new(
-            (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
-            &RED,
-        ))?
-        .label("y = x^2")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
-
-    chart
-        .configure_series_labels()
-        .background_style(&WHITE.mix(0.8))
-        .border_style(&BLACK)
-        .draw()?;
+            trajectory.iter().map(|state| (state.x, state.y)),
+            &RED
+        ))?;
 
     root.present()?;
 
