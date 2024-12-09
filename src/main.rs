0e7c8b5d4b998443bc662a7d42edb56cbcd8d284
Romain Desarzens <hib.pub@protonmail.com>
2024-12-09 15:41:26 +0000

    Refactor trajectory definition and plotting for improved clarity and performance

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use mpc_rs::mpc::controller::Controller;
+use mpc_rs::mpc::controller::Controller;
 use mpc_rs::mpc::simulator::system_simulate;
 
 use plotters::prelude::*;
@@ -137,101 +137,101 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .copy_from(&na::DMatrix::from_element(r, r, pred_weight));
     }
 
-    // let time_steps = 300;
+    let time_steps = 300;
 
-    // // Define a pulse trajectory
-    // let mut desired_traj = na::DMatrix::<f64>::zeros(time_steps, 1);
-    // desired_traj
-    //     .slice_mut((0..100, 0..1))
-    //     .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
-    // desired_traj
-    //     .slice_mut((200..300, 0..1))
-    //     .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
+    // Define a pulse trajectory
+    let mut desired_traj = na::DMatrix::<f64>::zeros(time_steps, 1);
+    desired_traj
+        .view_range_mut(0..100, 0..1)
+        .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
+    desired_traj
+        .view_range_mut(200..300, 0..1)
+        .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
 
-    // // Set the initial state
-    // let x0 = x0_test;
+    // Set the initial state
+    let x0 = x0_test;
 
-    // // Create the controller
-    // let mut mpc = Controller::new(
-    //     mat_a,
-    //     mat_b,
-    //     mat_c,
-    //     f,
-    //     v,
-    //     &mat_w3,
-    //     &mat_w4,
-    //     x0,
-    //     &desired_traj,
-    // )?;
+    // Create the controller
+    let mut mpc = Controller::new(
+        mat_a,
+        mat_b,
+        mat_c,
+        f,
+        v,
+        &mat_w3,
+        &mat_w4,
+        x0,
+        &desired_traj,
+    )?;
 
-    // for _ in 0..time_steps - f {
-    //     mpc.compute_control_inputs();
-    // }
+    for _ in 0..time_steps - f {
+        mpc.compute_control_inputs();
+    }
 
-    // {
-    //     let root = BitMapBackend::new("control.png", (800, 600)).into_drawing_area();
-    //     root.fill(&WHITE)?;
+    {
+        let root = BitMapBackend::new("control.png", (800, 600)).into_drawing_area();
+        root.fill(&WHITE)?;
 
-    //     let max_y = mpc
-    //         .outputs
-    //         .iter()
-    //         .cloned()
-    //         .fold(f64::NEG_INFINITY, f64::max);
-    //     let min_y = mpc.outputs.iter().cloned().fold(f64::INFINITY, f64::min);
-    //     let mut chart = ChartBuilder::on(&root)
-    //         .caption("System Output Y", ("sans-serif", 20))
-    //         .margin(10)
-    //         .x_label_area_size(30)
-    //         .y_label_area_size(40)
-    //         .build_cartesian_2d(0..y_test.ncols() as i32, min_y..max_y)?;
+        let max_y = mpc
+            .outputs
+            .iter()
+            .cloned()
+            .fold(f64::NEG_INFINITY, f64::max);
+        let min_y = mpc.outputs.iter().cloned().fold(f64::INFINITY, f64::min);
+        let mut chart = ChartBuilder::on(&root)
+            .caption("System Output Y", ("sans-serif", 20))
+            .margin(10)
+            .x_label_area_size(30)
+            .y_label_area_size(40)
+            .build_cartesian_2d(0..y_test.ncols() as i32, min_y..max_y)?;
 
-    //     chart.configure_mesh().draw()?;
+        chart.configure_mesh().draw()?;
 
-    //     // Plot input
-    //     let inputs_series: Vec<(i32, f64)> = mpc
-    //         .inputs
-    //         .row(0)
-    //         .iter()
-    //         .enumerate()
-    //         .map(|(i, &val)| (i as i32, val as f64))
-    //         .collect();
+        // Plot input
+        let inputs_series: Vec<(i32, f64)> = mpc
+            .inputs
+            .row(0)
+            .iter()
+            .enumerate()
+            .map(|(i, &val)| (i as i32, val as f64))
+            .collect();
 
-    //     chart
-    //         .draw_series(LineSeries::new(inputs_series, &Palette99::pick(0)))?
-    //         .label(format!("Input"))
-    //         .legend(move |(x, y)| PathElement::new([(x, y), (x + 20, y)], &Palette99::pick(0)));
+        chart
+            .draw_series(LineSeries::new(inputs_series, &Palette99::pick(0)))?
+            .label(format!("Input"))
+            .legend(move |(x, y)| PathElement::new([(x, y), (x + 20, y)], &Palette99::pick(0)));
 
-    //     // Plot system response
-    //     let outputs_serie: Vec<(i32, f64)> = mpc
-    //         .outputs
-    //         .iter()
-    //         .enumerate()
-    //         .map(|(i, &val)| (i as i32, val as f64))
-    //         .collect();
+        // Plot system response
+        let outputs_serie: Vec<(i32, f64)> = mpc
+            .outputs
+            .iter()
+            .enumerate()
+            .map(|(i, &val)| (i as i32, val as f64))
+            .collect();
 
-    //     chart
-    //         .draw_series(LineSeries::new(outputs_serie, &Palette99::pick(1)))?
-    //         .label(format!("Output"))
-    //         .legend(move |(x, y)| PathElement::new([(x, y), (x + 40, y)], &Palette99::pick(1)));
+        chart
+            .draw_series(LineSeries::new(outputs_serie, &Palette99::pick(1)))?
+            .label(format!("Output"))
+            .legend(move |(x, y)| PathElement::new([(x, y), (x + 40, y)], &Palette99::pick(1)));
 
-    //     let traj_series: Vec<(i32, f64)> = mpc
-    //         .desired_ctrl_traj_total
-    //         .iter()
-    //         .enumerate()
-    //         .map(|(i, &val)| (i as i32, val as f64))
-    //         .collect();
+        let traj_series: Vec<(i32, f64)> = mpc
+            .desired_ctrl_traj_total
+            .iter()
+            .enumerate()
+            .map(|(i, &val)| (i as i32, val as f64))
+            .collect();
 
-    //     chart
-    //         .draw_series(LineSeries::new(traj_series, &Palette99::pick(2)))?
-    //         .label(format!("Desired trajectory"))
-    //         .legend(move |(x, y)| PathElement::new([(x, y), (x + 60, y)], &Palette99::pick(2)));
+        chart
+            .draw_series(LineSeries::new(traj_series, &Palette99::pick(2)))?
+            .label(format!("Desired trajectory"))
+            .legend(move |(x, y)| PathElement::new([(x, y), (x + 60, y)], &Palette99::pick(2)));
 
-    //     chart
-    //         .configure_series_labels()
-    //         .background_style(&WHITE)
-    //         .border_style(&BLACK)
-    //         .draw()?;
-    // }
+        chart
+            .configure_series_labels()
+            .background_style(&WHITE)
+            .border_style(&BLACK)
+            .draw()?;
+    }
 
     Ok(())
 }
