0e3db9121a99c9dc00cff3e1716e1c33ca0af0b1
Romain Desarzens <hib.pub@protonmail.com>
2024-12-13 09:25:38 +0000

    Reorganize main

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,36 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let system_response = compute_system_response(&model, &input_test, &x0_test);
 
-    // Draw the response
+    // Define parameters
+    let f = 20usize;
+    let v = 20usize;
+
+    let time_steps = 300;
+
+    // Define a pulse trajectory
+    let mut desired_traj = na::DMatrix::<f64>::zeros(time_steps, 1);
+    desired_traj
+        .view_range_mut(0..100, 0..1)
+        .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
+    desired_traj
+        .view_range_mut(200..300, 0..1)
+        .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
+
+    // Set the initial state
+    let x0 = x0_test;
+
+    // Create the controller
+    let pred_weight = 10f64;
+    let q0 = 0.0000000011f64;
+    let q_other = 0.0001f64;
+
+    let mut mpc = Controller::new(&model, f, v, q0, q_other, pred_weight, x0, &desired_traj)?;
+
+    for _ in 0..time_steps - f {
+        mpc.compute_control_inputs();
+    }
+
+    // Draw the step response
     {
         let root = BitMapBackend::new("step_response.png", (800, 600)).into_drawing_area();
         root.fill(&WHITE)?;
@@ -75,35 +104,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .draw()?;
     }
 
-    // Define parameters
-    let f = 20usize;
-    let v = 20usize;
-
-    let time_steps = 300;
-    
-    // Define a pulse trajectory
-    let mut desired_traj = na::DMatrix::<f64>::zeros(time_steps, 1);
-    desired_traj
-        .view_range_mut(0..100, 0..1)
-        .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
-    desired_traj
-        .view_range_mut(200..300, 0..1)
-        .copy_from(&na::DMatrix::from_element(100, 1, 1.0));
-
-    // Set the initial state
-    let x0 = x0_test;
-
-    // Create the controller
-    let pred_weight = 10f64;
-    let q0 = 0.0000000011f64;
-    let q_other = 0.0001f64;
-
-    let mut mpc = Controller::new(&model, f, v, q0, q_other, pred_weight, x0, &desired_traj)?;
-
-    for _ in 0..time_steps - f {
-        mpc.compute_control_inputs();
-    }
-
+    // Draw the control
     {
         let root = BitMapBackend::new("control.png", (800, 600)).into_drawing_area();
         root.fill(&WHITE)?;
